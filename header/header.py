2188cac81e822b1dca6cd4fa1c7249f5fff7ae13
Ytalo Campos <ytalo.campos0@gmail.com>
2019-08-04 00:58:22 -0300

    update header

--- header/header.py
+++ header/header.py
@@ -1,20 +1,27 @@
 import time
 
 def header():
-    x = "||||||||||||||||||||||||||"
+    main = """
+      _____     _      ____     _          ____       _____
+     / ____|   | |    / ___|   | |        / __ \     / ____|
+    / /        | |   / /       | |       / /  \ \   / /
+    \ \___     | |   \ \__     | |      | |    | |  | |   ___
+     \___ \    | |    \__  \   | |      | |    | |  | |  |_  |
+         \ \   | |        \ \  | |      | |    | |  | |    | |
+     ____/ /   | |    ____/ /  | |____   \ \__/ /   \ \___/ /
+    |_____/    |_|   |_____/   |______|   \____/     \_____/
 
-    print(x)
-    print("     SIMPLE KEYLLOGER")
-    print(x)
+
+                                                Powered by Ytalo Campos.""" 
+
+    print(main)
+    time.sleep(2)         
+
+    print("--from - sender email")
+    print("--to - recipient email")
+    print("--pass - sender password\n")
     time.sleep(1)
 
-    print(" --from - sender email")
-    print(" --to - recipient email")
-    print(" --pass - sender password")
-    print(x)
-    time.sleep(1)
-    print(" > press 'F4' to receive\n the data by email\n")
-    print(" > press 'ESC' to close")
-    print(x)
-    
-    print('loading...')
+    print("> press 'F4' to receive the data by email")
+    print("> press 'ESC' to close\n")
+    print("loading...")
